7d8d8314a0a9c5e12a48cb92803cc82a6d9abace
Liliana Emfinger <lildev0ps1@gmail.com>
2020-06-24 10:38:03 -0700

    added js

--- quiz.js
+++ quiz.js
@@ -1,4 +1,4 @@
-'use strict';
+//'use strict';
 // The starting screen should have a button that users can click to start the quiz.
 // Users should be prompted through a series of at least 5 multiple choice questions that they can answer.
 // Users should be asked questions 1 after the other.
@@ -11,3 +11,33 @@
 // Users should be shown their overall score at the end of the quiz. In other words, how many questions they got right out of the total questions asked.
 // Users should be able to start a new quiz.
 
+const STORE = {
+    questions: [//1
+      {
+        question: "What are heading elements for?",
+        options: [
+          "It tells where the head of a document is", 
+          "For styling purposes", 
+          "To establish information hierarachy", 
+          "To establish authority"
+        ],
+        answer: "To establish information hierarachy"
+      },
+
+
+    ]};
+
+
+    function startQuiz() {
+        $('#start-b').click(function(){
+            $('.startpage').addClass('hide')
+            $('.question').removeClass('hide')
+        });
+    };
+   
+
+    function initQuiz() {
+        startQuiz();
+    }
+
+    $(initQuiz);
